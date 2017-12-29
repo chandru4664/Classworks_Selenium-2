2bec44849037576b42f4693ddbebd89cf957b1cc
HTC Training <31919061+HTCTraining@users.noreply.github.com>
2017-12-29 10:47:34 +0530

    Add files via upload

---
+++ Day11/java.bat
@@ -0,0 +1,4 @@
+SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_131
+SET JRE_HOME=C:\Program Files\Java\jre1.8.0_131
+SET CATALINA_HOME=D:\DevOps\apache-tomcat-8.5.15
+SET PATH=%PATH%;%JAVA_HOME%\bin
---
+++ Day11/maven.bat
@@ -0,0 +1,5 @@
+SET M2_HOME=D:\DevOps\apache-maven-3.3.9
+set M2=%M2_HOME%\bin
+set MAVEN_OPTS=-Xms256m -Xmx512m
+
+SET PATH=%PATH%;%M2%
